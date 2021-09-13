5b35537e23fd1405304d2bcaf95d615a051ade0b
Duckson29 <50597214+Duckson29@users.noreply.github.com>
2021-09-13 15:21:33 +0200

    create new sln, adde 3 project (mobil,route,Database projects)

--- MobilEndPointGrpcService/Program.cs
+++ MobilEndPointGrpcService/Program.cs
@@ -15,7 +15,7 @@ namespace MobilEndPointGrpcService
             CreateHostBuilder(args).Build().Run();
         }
 
-        // Additional configuration is required to successfully run gRPC on macOS.
+        // Additional configuration is required to successfully run gRPC on macOS. 
         // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
