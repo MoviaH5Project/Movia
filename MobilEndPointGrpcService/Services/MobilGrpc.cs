aaefd26b996fad571d33cbf1b3a0831f6850f5fd
Andi Olle Olsen <andi0137@zbc.dk>
2021-09-17 11:53:52 +0200

    Change proto string id to int32 id

--- MobilEndPointGrpcService/Services/MobilGrpc.cs
+++ MobilEndPointGrpcService/Services/MobilGrpc.cs
@@ -8,26 +8,49 @@ using Google.Protobuf;
 using MobilEndPointGrpcService;
 using MobilEndPointGrpcService.Protos;
 using Grpc.Net.Client;
+using database = DatabaseGrpcService.Protos;
+using System.Net.Http;
 
 namespace MobilEndPointGrpcService.Services
 {
-    public class MobilGrpc : MobilEndPointGrpcService.Protos.BusGrpcService.BusGrpcServiceBase
+    /// <summary>
+    /// This services is is the endpoint for the mobil application.
+    /// </summary>
+    public class MobilGrpc : BusGrpcService.BusGrpcServiceBase
     {
-        //public static GrpcChannel channel = GrpcChannel();
-
+        #region Props
+        static database.DatabaseGrpcService.DatabaseGrpcServiceClient channel;
         private readonly ILogger<MobilGrpc> _logger;
+        #endregion
+        #region ctor
         public MobilGrpc(ILogger<MobilGrpc> logger)
         {
             _logger = logger;
+            //if (channel == null)
+            //    channel = new database.DatabaseGrpcService.DatabaseGrpcServiceClient(GrpcChannel.ForAddress("0.0.0.0", new GrpcChannelOptions
+            //    {
+            //        Credentials = ChannelCredentials.Insecure
+            //    }));
+
         }
-        //Stuff
+        #endregion
+        #region Grpc_Methoeds
         public override Task<BusList> GetAllBuss(BusRequest request, ServerCallContext context)
         {
-            
-            return base.GetAllBuss(request, context);
+            //byte[] result = channel.GetAllBusses(new database.Request { Id = 1 }).ToByteArray();
+
+            //BusList repley = BusList.Parser.ParseFrom(result);
+
+            return Task.FromResult(new BusList());
+            //return Task.FromResult(repley);
         }
         public override Task<DatabaseChagedBus> UpdatePassagseCount(Bus request, ServerCallContext context)
         {
+            Console.WriteLine($"{context.Host} called: UpdatePassagseCount");
+            byte[] result = channel.GetAllBusses(new database.Request { Id = 1 }).ToByteArray();
+
+            BusList repley = BusList.Parser.ParseFrom(result);
+
             return base.UpdatePassagseCount(request, context);
         }
         public override Task<DatabaseChagedBus> CreateBus(Bus request, ServerCallContext context)
@@ -41,7 +64,15 @@ namespace MobilEndPointGrpcService.Services
 
         public override Task<Bus> GetBusInfo(BusRequest request, ServerCallContext context)
         {
-            return base.GetBusInfo(request, context);
+            Console.WriteLine("bus infomation sent");            
+            //Database local object
+            //database.Request datarequst = new database.Request();
+            //datarequst.Id = request.Id;
+            //MobilService Proto Object converting
+            //Bus.Parser.ParseFrom(channel.GetBus(datarequst).ToByteArray());
+
+            return Task.FromResult(new Bus {Driver="Andi",Id=1,Make="Bmw",Name="asdf" });
+            
         }
 
         public override Task<DatabaseChagedBus> UpdaeBusPoscition(Bus request, ServerCallContext context)
@@ -52,5 +83,6 @@ namespace MobilEndPointGrpcService.Services
         {
             return base.UpdateBusInfo(request, context);
         }
+        #endregion
     }
 }
