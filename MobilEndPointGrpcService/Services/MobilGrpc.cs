d7b0e37e0a470c2bd0280995f056f5185601fe3a
Duckson29 <50597214+Duckson29@users.noreply.github.com>
2021-09-17 09:22:10 +0200

    fix crossrefs

--- MobilEndPointGrpcService/Services/MobilGrpc.cs
+++ MobilEndPointGrpcService/Services/MobilGrpc.cs
@@ -9,7 +9,7 @@ using MoviaMobilEndPiontGrpc.Protos;
 
 namespace MoviaMobilEndPiontGrpc.Services
 {
-    public class MobilGrpc : MoviaMobilEndPiontGrpc.Protos.BusGrpcService.BusGrpcServiceBase
+    public class MobilGrpc
     {
         private readonly ILogger<MobilGrpc> _logger;
         public MobilGrpc(ILogger<MobilGrpc> logger)
@@ -17,39 +17,8 @@ namespace MoviaMobilEndPiontGrpc.Services
             _logger = logger;
         }
         //Stuff
-        public override Task<DatabaseChaged> UpdaeBusPoscition(Bus request, ServerCallContext context)
-        {
-            //requste => databaseservice ( requste => Bus ) => database (Bus);
-            return base.UpdaeBusPoscition(request, context);
-        }
-        public override Task<DatabaseChaged> UpdateBusInfo(Bus request, ServerCallContext context)
-        {
-            //Bus => update = RPC UpdateBus(Bus) => database 
-            // Requste => databaseservice(Bus) = database (Bus)
-            return base.UpdateBusInfo(request, context);
-        }
-        public override Task<Bus> GetBusInfo(BusRequest request, ServerCallContext context)
-        {
-            return base.GetBusInfo(request, context);
-        }
+        
+      
 
-        public override Task<DatabaseChaged> CreateBus(Bus request, ServerCallContext context)
-        {
-            return base.CreateBus(request, context);
-        }
-        public override Task<DatabaseChaged> DeleteBus(Bus request, ServerCallContext context)
-        {
-            return base.DeleteBus(request, context);
-        }
-
-        public override Task<BusList> GetAllBuss(BusRequest request, ServerCallContext context)
-        {
-            return base.GetAllBuss(request, context);
-        }
-
-        public override Task<DatabaseChaged> UpdatePassagseCount(Bus request, ServerCallContext context)
-        {
-            return base.UpdatePassagseCount(request, context);
-        }
     }
 }
