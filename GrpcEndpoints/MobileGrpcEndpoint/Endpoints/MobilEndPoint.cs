baaa2c1ae9e14b54f2e5213b0abd88a3bfe1c9a8
Duckson29 <50597214+Duckson29@users.noreply.github.com>
2021-09-24 13:50:57 +0200

    branch fixing

---
+++ GrpcEndpoints/MobileGrpcEndpoint/Endpoints/MobilEndPoint.cs
@@ -0,0 +1,31 @@
+﻿using Grpc.Core;
+using MobileGrpcEndpoint.Protos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MobileGrpcEndpoint.Endpoints
+{
+    /// <summary>
+    /// This is the expose methodes to the mobil application.
+    /// </summary>
+    public class MobilEndPoint : MobileGrpcEndpoint.Protos.MobileGrpcEndpoint.MobileGrpcEndpointBase
+    {
+        #region Grpc_Methoeds
+        public override Task<DatabaseChagedTicket> CreateTicket(TicketRequest request, ServerCallContext context)
+        {
+            return base.CreateTicket(request, context);
+        }
+        public override Task<DatabaseChagedTicket> RemoveTicket(TicketRequest request, ServerCallContext context)
+        {
+            return base.RemoveTicket(request, context);
+        }
+        public override Task<Ticket> Getticket(TicketRequest request, ServerCallContext context)
+        {
+            return base.Getticket(request, context);
+        }
+
+        #endregion
+    }
+}
