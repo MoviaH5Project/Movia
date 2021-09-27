977175f887de3f460148b3c96533f6181fd9a34e
Martin Rosenquist <71012256+Mart994f@users.noreply.github.com>
2021-09-27 11:12:40 +0200

    Implemented bus service (#10)
    Co-authored-by: Martin Rosenquist <martin.rosenquist@visma.com>

---
+++ GrpcServices/BusGrpcService/ApplicationServices/BusGrpcService.cs
@@ -0,0 +1,155 @@
+﻿using BusGrpcService.Helpers;
+using BusGrpcService.InfrastructureServices;
+using Google.Protobuf;
+using Grpc.Core;
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using Database = DatabaseGrpcService.Protos;
+
+namespace BusGrpcService.ApplicationServices
+{
+	internal class BusGrpcService : Protos.BusGrpcService.BusGrpcServiceBase
+	{
+		private const string _serviceContainerName = "bus_service";
+
+		private readonly IDatabaseGrpcService _databaseGrpcService;
+		private readonly ILogHelper<BusGrpcService> _logHelper;
+
+		public BusGrpcService(IDatabaseGrpcService databaseGrpcService, ILogHelper<BusGrpcService> logHelper)
+		{
+			_databaseGrpcService = databaseGrpcService;
+			_logHelper = logHelper ?? throw new ArgumentNullException(nameof(logHelper));
+		}
+
+		public override async Task<Protos.Response> CreateBus(Protos.Bus request, ServerCallContext context)
+		{
+			if (request is null)
+			{
+				_logHelper.LogNullException(nameof(request));
+				throw new ArgumentNullException(nameof(request));
+			}
+
+			if (context is null)
+			{
+				_logHelper.LogNullException(nameof(context));
+				throw new ArgumentNullException(nameof(context));
+			}
+
+			try
+			{
+				_logHelper.LogInvokingGrpcMethod(MethodBase.GetCurrentMethod().Name, _serviceContainerName, request);
+				return Protos.Response.Parser.ParseFrom((await _databaseGrpcService.CreateBusAsync(Database.Bus.Parser.ParseFrom(request.ToByteArray()))).ToByteArray());
+			}
+			catch (Exception ex)
+			{
+				_logHelper.LogErrorInvokingGrpcMethod(ex, MethodBase.GetCurrentMethod().Name, _serviceContainerName, request);
+				throw;
+			}
+		}
+
+		public override async Task<Protos.Bus> GetBus(Protos.Request request, ServerCallContext context)
+		{
+			if (request is null)
+			{
+				_logHelper.LogNullException(nameof(request));
+				throw new ArgumentNullException(nameof(request));
+			}
+
+			if (context is null)
+			{
+				_logHelper.LogNullException(nameof(context));
+				throw new ArgumentNullException(nameof(context));
+			}
+
+			try
+			{
+				_logHelper.LogInvokingGrpcMethod(MethodBase.GetCurrentMethod().Name, _serviceContainerName, request);
+				return Protos.Bus.Parser.ParseFrom((await _databaseGrpcService.GetBusAsync(Database.Request.Parser.ParseFrom(request.ToByteArray()))).ToByteArray());
+			}
+			catch (Exception ex)
+			{
+				_logHelper.LogErrorInvokingGrpcMethod(ex, MethodBase.GetCurrentMethod().Name, _serviceContainerName, request);
+				throw;
+			}
+		}
+
+		public override async Task<Protos.BusList> GetAllBusses(Protos.Request request, ServerCallContext context)
+		{
+			if (request is null)
+			{
+				_logHelper.LogNullException(nameof(request));
+				throw new ArgumentNullException(nameof(request));
+			}
+
+			if (context is null)
+			{
+				_logHelper.LogNullException(nameof(context));
+				throw new ArgumentNullException(nameof(context));
+			}
+
+			try
+			{
+				_logHelper.LogInvokingGrpcMethod(MethodBase.GetCurrentMethod().Name, _serviceContainerName, request);
+				return Protos.BusList.Parser.ParseFrom((await _databaseGrpcService.GetAllBussesAsync(Database.Request.Parser.ParseFrom(request.ToByteArray()))).ToByteArray());
+			}
+			catch (Exception ex)
+			{
+				_logHelper.LogErrorInvokingGrpcMethod(ex, MethodBase.GetCurrentMethod().Name, _serviceContainerName, request);
+				throw;
+			}
+		}
+
+		public override async Task<Protos.Response> UpdateBus(Protos.Bus request, ServerCallContext context)
+		{
+			if (request is null)
+			{
+				_logHelper.LogNullException(nameof(request));
+				throw new ArgumentNullException(nameof(request));
+			}
+
+			if (context is null)
+			{
+				_logHelper.LogNullException(nameof(context));
+				throw new ArgumentNullException(nameof(context));
+			}
+
+			try
+			{
+				_logHelper.LogInvokingGrpcMethod(MethodBase.GetCurrentMethod().Name, _serviceContainerName, request);
+				return Protos.Response.Parser.ParseFrom((await _databaseGrpcService.UpdateBusAsync(Database.Bus.Parser.ParseFrom(request.ToByteArray()))).ToByteArray());
+			}
+			catch (Exception ex)
+			{
+				_logHelper.LogErrorInvokingGrpcMethod(ex, MethodBase.GetCurrentMethod().Name, _serviceContainerName, request);
+				throw;
+			}
+		}
+
+		public override async Task<Protos.Response> DeleteBus(Protos.Request request, ServerCallContext context)
+		{
+			if (request is null)
+			{
+				_logHelper.LogNullException(nameof(request));
+				throw new ArgumentNullException(nameof(request));
+			}
+
+			if (context is null)
+			{
+				_logHelper.LogNullException(nameof(context));
+				throw new ArgumentNullException(nameof(context));
+			}
+
+			try
+			{
+				_logHelper.LogInvokingGrpcMethod(MethodBase.GetCurrentMethod().Name, _serviceContainerName, request);
+				return Protos.Response.Parser.ParseFrom((await _databaseGrpcService.DeleteBusAsync(Database.Request.Parser.ParseFrom(request.ToByteArray()))).ToByteArray());
+			}
+			catch (Exception ex)
+			{
+				_logHelper.LogErrorInvokingGrpcMethod(ex, MethodBase.GetCurrentMethod().Name, _serviceContainerName, request);
+				throw;
+			}
+		}
+	}
+}
---
+++ GrpcServices/BusGrpcService/Helpers/LogHelper.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+
+namespace BusGrpcService.Helpers
+{
+	public interface ILogHelper<T>
+	{
+		void LogErrorInvokingGrpcMethod(Exception exception, string methodName, string containerName, object argument);
+		void LogInvokingGrpcMethod(string methodName, string containerName, object argument);
+		void LogNullException(string argumentName);
+	}
+
+	public class LogHelper<T> : ILogHelper<T>
+	{
+		private readonly ILogger<T> _logger;
+
+		public LogHelper(ILogger<T> logger)
+		{
+			_logger = logger ?? throw new ArgumentNullException(nameof(logger));
+		}
+
+		public void LogInvokingGrpcMethod(string methodName, string containerName, object argument)
+		{
+			_logger.LogInformation("Invoking grpc {methodName} on {containerName}. Argument {@argument}", methodName, containerName, argument);
+		}
+
+		public void LogErrorInvokingGrpcMethod(Exception exception, string methodName, string containerName, object argument)
+		{
+			_logger.LogError(exception, "An unexpected error occurred when invoking grpc {methodName} on {containerName}. Argument {@argumenr}", methodName, containerName, argument);
+		}
+
+		public void LogNullException(string argumentName)
+		{
+			_logger.LogCritical("The argument {argumentName} was null", argumentName);
+		}
+	}
+}
