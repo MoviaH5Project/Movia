71feeb888cfa6c8f79773f06fbfedbdc6dedfeae
Martin Rosenquist <martin.rosenquist@visma.com>
2021-09-27 10:17:23 +0200

    Implemented route service

--- GrpcServices/RouteGrpcService/Startup.cs
+++ GrpcServices/RouteGrpcService/Startup.cs
@@ -1,37 +1,49 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Grpc.Net.Client;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using RouteGrpcService.Helpers;
+using RouteGrpcService.InfrastructureServices;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Database = DatabaseGrpcService.Protos;
 
 namespace RouteGrpcService
 {
-    public class Startup
-    {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddGrpc();
-        }
+	public class Startup
+	{
+		// This method gets called by the runtime. Use this method to add services to the container.
+		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddSingleton<Database.DatabaseGrpcService.DatabaseGrpcServiceClient>(services =>
+			{
+				return new Database.DatabaseGrpcService.DatabaseGrpcServiceClient(
+					GrpcChannel.ForAddress(Environment.GetEnvironmentVariable("DATABASE_SERVICE_URL"),
+					new GrpcChannelOptions
+					{
+						Credentials = Grpc.Core.ChannelCredentials.Insecure
+					}));
+			});
+			services.AddTransient<IDatabaseGrpcService, InfrastructureServices.DatabaseGrpcService>();
+			services.AddTransient(typeof(ILogHelper<>), typeof(LogHelper<>));
+			services.AddGrpc();
+		}
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
 
-            app.UseRouting();
+			app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
-            {
-            });
-        }
-    }
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapGrpcService<ApplicationServices.RouteGrpcService>();
+			});
+		}
+	}
 }
