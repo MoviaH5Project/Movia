4acff16de58de6fb20b4faab6e5861a581e92396
Martin Rosenquist <71012256+Mart994f@users.noreply.github.com>
2021-09-27 11:14:11 +0200

    Implemented route service (#11)
    Co-authored-by: Martin Rosenquist <martin.rosenquist@visma.com>

---
+++ GrpcServices/RouteGrpcService/ApplicationServices/RouteGrpcService.cs
@@ -0,0 +1,285 @@
+﻿using Google.Protobuf;
+using Grpc.Core;
+using RouteGrpcService.Helpers;
+using RouteGrpcService.InfrastructureServices;
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using Database = DatabaseGrpcService.Protos;
+
+namespace RouteGrpcService.ApplicationServices
+{
+	internal class RouteGrpcService : Protos.RouteGrpcService.RouteGrpcServiceBase
+	{
+		private const string _serviceContainerName = "route_service";
+
+		private readonly IDatabaseGrpcService _databaseGrpcService;
+		private readonly ILogHelper<RouteGrpcService> _logHelper;
+
+		internal RouteGrpcService(IDatabaseGrpcService databaseGrpcService, ILogHelper<RouteGrpcService> logHelper)
+		{
+			_databaseGrpcService = databaseGrpcService ?? throw new ArgumentNullException(nameof(databaseGrpcService));
+			_logHelper = logHelper ?? throw new ArgumentNullException(nameof(logHelper));
+		}
+
+		public override async Task<Protos.Response> CreateRoute(Protos.Route request, ServerCallContext context)
+		{
+			if (request is null)
+			{
+				_logHelper.LogNullException(nameof(request));
+				throw new ArgumentNullException(nameof(request));
+			}
+
+			if (context is null)
+			{
+				_logHelper.LogNullException(nameof(context));
+				throw new ArgumentNullException(nameof(context));
+			}
+
+			try
+			{
+				_logHelper.LogInvokingGrpcMethod(MethodBase.GetCurrentMethod().Name, _serviceContainerName, request);
+				return Protos.Response.Parser.ParseFrom((await _databaseGrpcService.CreateRouteAsync(Database.Route.Parser.ParseFrom(request.ToByteArray()))).ToByteArray());
+			}
+			catch (Exception ex)
+			{
+				_logHelper.LogErrorInvokingGrpcMethod(ex, MethodBase.GetCurrentMethod().Name, _serviceContainerName, request);
+				throw;
+			}
+		}
+
+		public override async Task<Protos.Route> GetRoute(Protos.Request request, ServerCallContext context)
+		{
+			if (request is null)
+			{
+				_logHelper.LogNullException(nameof(request));
+				throw new ArgumentNullException(nameof(request));
+			}
+
+			if (context is null)
+			{
+				_logHelper.LogNullException(nameof(context));
+				throw new ArgumentNullException(nameof(context));
+			}
+
+			try
+			{
+				_logHelper.LogInvokingGrpcMethod(MethodBase.GetCurrentMethod().Name, _serviceContainerName, request);
+				return Protos.Route.Parser.ParseFrom((await _databaseGrpcService.GetRouteAsync(Database.Request.Parser.ParseFrom(request.ToByteArray()))).ToByteArray());
+			}
+			catch (Exception ex)
+			{
+				_logHelper.LogErrorInvokingGrpcMethod(ex, MethodBase.GetCurrentMethod().Name, _serviceContainerName, request);
+				throw;
+			}
+		}
+
+		public override async Task<Protos.RouteList> GetAllRoutes(Protos.Request request, ServerCallContext context)
+		{
+			if (request is null)
+			{
+				_logHelper.LogNullException(nameof(request));
+				throw new ArgumentNullException(nameof(request));
+			}
+
+			if (context is null)
+			{
+				_logHelper.LogNullException(nameof(context));
+				throw new ArgumentNullException(nameof(context));
+			}
+
+			try
+			{
+				_logHelper.LogInvokingGrpcMethod(MethodBase.GetCurrentMethod().Name, _serviceContainerName, request);
+				return Protos.RouteList.Parser.ParseFrom((await _databaseGrpcService.GetAllRoutesAsync(Database.Request.Parser.ParseFrom(request.ToByteArray()))).ToByteArray());
+			}
+			catch (Exception ex)
+			{
+				_logHelper.LogErrorInvokingGrpcMethod(ex, MethodBase.GetCurrentMethod().Name, _serviceContainerName, request);
+				throw;
+			}
+		}
+
+		public override async Task<Protos.Response> UpdateRoute(Protos.Route request, ServerCallContext context)
+		{
+			if (request is null)
+			{
+				_logHelper.LogNullException(nameof(request));
+				throw new ArgumentNullException(nameof(request));
+			}
+
+			if (context is null)
+			{
+				_logHelper.LogNullException(nameof(context));
+				throw new ArgumentNullException(nameof(context));
+			}
+
+			try
+			{
+				_logHelper.LogInvokingGrpcMethod(MethodBase.GetCurrentMethod().Name, _serviceContainerName, request);
+				return Protos.Response.Parser.ParseFrom((await _databaseGrpcService.UpdateRouteAsync(Database.Route.Parser.ParseFrom(request.ToByteArray()))).ToByteArray());
+			}
+			catch (Exception ex)
+			{
+				_logHelper.LogErrorInvokingGrpcMethod(ex, MethodBase.GetCurrentMethod().Name, _serviceContainerName, request);
+				throw;
+			}
+		}
+
+		public override async Task<Protos.Response> DeleteRoute(Protos.Request request, ServerCallContext context)
+		{
+			if (request is null)
+			{
+				_logHelper.LogNullException(nameof(request));
+				throw new ArgumentNullException(nameof(request));
+			}
+
+			if (context is null)
+			{
+				_logHelper.LogNullException(nameof(context));
+				throw new ArgumentNullException(nameof(context));
+			}
+
+			try
+			{
+				_logHelper.LogInvokingGrpcMethod(MethodBase.GetCurrentMethod().Name, _serviceContainerName, request);
+				return Protos.Response.Parser.ParseFrom((await _databaseGrpcService.DeleteRouteAsync(Database.Request.Parser.ParseFrom(request.ToByteArray()))).ToByteArray());
+			}
+			catch (Exception ex)
+			{
+				_logHelper.LogErrorInvokingGrpcMethod(ex, MethodBase.GetCurrentMethod().Name, _serviceContainerName, request);
+				throw;
+			}
+		}
+
+		public override async Task<Protos.Response> CreateStop(Protos.Stop request, ServerCallContext context)
+		{
+			if (request is null)
+			{
+				_logHelper.LogNullException(nameof(request));
+				throw new ArgumentNullException(nameof(request));
+			}
+
+			if (context is null)
+			{
+				_logHelper.LogNullException(nameof(context));
+				throw new ArgumentNullException(nameof(context));
+			}
+
+			try
+			{
+				_logHelper.LogInvokingGrpcMethod(MethodBase.GetCurrentMethod().Name, _serviceContainerName, request);
+				return Protos.Response.Parser.ParseFrom((await _databaseGrpcService.CreateStopAsync(Database.Stop.Parser.ParseFrom(request.ToByteArray()))).ToByteArray());
+			}
+			catch (Exception ex)
+			{
+				_logHelper.LogErrorInvokingGrpcMethod(ex, MethodBase.GetCurrentMethod().Name, _serviceContainerName, request);
+				throw;
+			}
+		}
+
+		public override async Task<Protos.Stop> GetStop(Protos.Request request, ServerCallContext context)
+		{
+			if (request is null)
+			{
+				_logHelper.LogNullException(nameof(request));
+				throw new ArgumentNullException(nameof(request));
+			}
+
+			if (context is null)
+			{
+				_logHelper.LogNullException(nameof(context));
+				throw new ArgumentNullException(nameof(context));
+			}
+
+			try
+			{
+				_logHelper.LogInvokingGrpcMethod(MethodBase.GetCurrentMethod().Name, _serviceContainerName, request);
+				return Protos.Stop.Parser.ParseFrom((await _databaseGrpcService.GetRouteAsync(Database.Request.Parser.ParseFrom(request.ToByteArray()))).ToByteArray());
+			}
+			catch (Exception ex)
+			{
+				_logHelper.LogErrorInvokingGrpcMethod(ex, MethodBase.GetCurrentMethod().Name, _serviceContainerName, request);
+				throw;
+			}
+		}
+
+		public override async Task<Protos.StopList> GetAllStops(Protos.Request request, ServerCallContext context)
+		{
+			if (request is null)
+			{
+				_logHelper.LogNullException(nameof(request));
+				throw new ArgumentNullException(nameof(request));
+			}
+
+			if (context is null)
+			{
+				_logHelper.LogNullException(nameof(context));
+				throw new ArgumentNullException(nameof(context));
+			}
+
+			try
+			{
+				_logHelper.LogInvokingGrpcMethod(MethodBase.GetCurrentMethod().Name, _serviceContainerName, request);
+				return Protos.StopList.Parser.ParseFrom((await _databaseGrpcService.GetAllStopsAsync(Database.Request.Parser.ParseFrom(request.ToByteArray()))).ToByteArray());
+			}
+			catch (Exception ex)
+			{
+				_logHelper.LogErrorInvokingGrpcMethod(ex, MethodBase.GetCurrentMethod().Name, _serviceContainerName, request);
+				throw;
+			}
+		}
+
+		public override async Task<Protos.Response> UpdateStop(Protos.Stop request, ServerCallContext context)
+		{
+			if (request is null)
+			{
+				_logHelper.LogNullException(nameof(request));
+				throw new ArgumentNullException(nameof(request));
+			}
+
+			if (context is null)
+			{
+				_logHelper.LogNullException(nameof(context));
+				throw new ArgumentNullException(nameof(context));
+			}
+
+			try
+			{
+				_logHelper.LogInvokingGrpcMethod(MethodBase.GetCurrentMethod().Name, _serviceContainerName, request);
+				return Protos.Response.Parser.ParseFrom((await _databaseGrpcService.UpdateStopAsync(Database.Stop.Parser.ParseFrom(request.ToByteArray()))).ToByteArray());
+			}
+			catch (Exception ex)
+			{
+				_logHelper.LogErrorInvokingGrpcMethod(ex, MethodBase.GetCurrentMethod().Name, _serviceContainerName, request);
+				throw;
+			}
+		}
+
+		public override async Task<Protos.Response> DeleteStop(Protos.Request request, ServerCallContext context)
+		{
+			if (request is null)
+			{
+				_logHelper.LogNullException(nameof(request));
+				throw new ArgumentNullException(nameof(request));
+			}
+
+			if (context is null)
+			{
+				_logHelper.LogNullException(nameof(context));
+				throw new ArgumentNullException(nameof(context));
+			}
+
+			try
+			{
+				_logHelper.LogInvokingGrpcMethod(MethodBase.GetCurrentMethod().Name, _serviceContainerName, request);
+				return Protos.Response.Parser.ParseFrom((await _databaseGrpcService.DeleteStopAsync(Database.Request.Parser.ParseFrom(request.ToByteArray()))).ToByteArray());
+			}
+			catch (Exception ex)
+			{
+				_logHelper.LogErrorInvokingGrpcMethod(ex, MethodBase.GetCurrentMethod().Name, _serviceContainerName, request);
+				throw;
+			}
+		}
+	}
+}
---
+++ GrpcServices/RouteGrpcService/InfrastructureServices/DatabaseGrpcService.cs
@@ -0,0 +1,238 @@
+﻿using RouteGrpcService.Helpers;
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using Database = DatabaseGrpcService.Protos;
+
+namespace RouteGrpcService.InfrastructureServices
+{
+	internal interface IDatabaseGrpcService
+	{
+		Task<Database.Response> CreateRouteAsync(Database.Route request);
+		Task<Database.Response> CreateStopAsync(Database.Stop request);
+		Task<Database.Response> DeleteRouteAsync(Database.Request request);
+		Task<Database.Response> DeleteStopAsync(Database.Request request);
+		Task<Database.RouteList> GetAllRoutesAsync(Database.Request request);
+		Task<Database.StopList> GetAllStopsAsync(Database.Request request);
+		Task<Database.Route> GetRouteAsync(Database.Request request);
+		Task<Database.Stop> GetStopAsync(Database.Request request);
+		Task<Database.Response> UpdateRouteAsync(Database.Route request);
+		Task<Database.Response> UpdateStopAsync(Database.Stop request);
+	}
+
+	internal class DatabaseGrpcService : IDatabaseGrpcService
+	{
+		private const string _serviceContainerName = "database_service";
+
+		private readonly Database.DatabaseGrpcService.DatabaseGrpcServiceClient _databaseGrpcServiceClient;
+		private readonly ILogHelper<DatabaseGrpcService> _logHelper;
+
+		public DatabaseGrpcService(
+			Database.DatabaseGrpcService.DatabaseGrpcServiceClient databaseGrpcServiceClient,
+			ILogHelper<DatabaseGrpcService> logHelper)
+		{
+			_databaseGrpcServiceClient = databaseGrpcServiceClient ?? throw new ArgumentNullException(nameof(databaseGrpcServiceClient));
+			_logHelper = logHelper ?? throw new ArgumentNullException(nameof(logHelper));
+		}
+
+		public async Task<Database.Response> CreateRouteAsync(Database.Route request)
+		{
+			if (request is null)
+			{
+				_logHelper.LogNullException(nameof(request));
+				throw new ArgumentNullException(nameof(request));
+			}
+
+			try
+			{
+				_logHelper.LogInvokingGrpcMethod(MethodBase.GetCurrentMethod().Name, _serviceContainerName, request);
+				return await _databaseGrpcServiceClient.CreateRouteAsync(request);
+			}
+			catch (Exception ex)
+			{
+				_logHelper.LogErrorInvokingGrpcMethod(ex, MethodBase.GetCurrentMethod().Name, _serviceContainerName, request);
+				throw;
+			}
+		}
+
+		public async Task<Database.Route> GetRouteAsync(Database.Request request)
+		{
+			if (request is null)
+			{
+				_logHelper.LogNullException(nameof(request));
+				throw new ArgumentNullException(nameof(request));
+			}
+
+			try
+			{
+				_logHelper.LogInvokingGrpcMethod(MethodBase.GetCurrentMethod().Name, _serviceContainerName, request);
+				return await _databaseGrpcServiceClient.GetRouteAsync(request);
+			}
+			catch (Exception ex)
+			{
+				_logHelper.LogErrorInvokingGrpcMethod(ex, MethodBase.GetCurrentMethod().Name, _serviceContainerName, request);
+				throw;
+			}
+		}
+
+		public async Task<Database.RouteList> GetAllRoutesAsync(Database.Request request)
+		{
+			if (request is null)
+			{
+				_logHelper.LogNullException(nameof(request));
+				throw new ArgumentNullException(nameof(request));
+			}
+
+			try
+			{
+				_logHelper.LogInvokingGrpcMethod(MethodBase.GetCurrentMethod().Name, _serviceContainerName, request);
+				return await _databaseGrpcServiceClient.GetAllRoutesAsync(request);
+			}
+			catch (Exception ex)
+			{
+				_logHelper.LogErrorInvokingGrpcMethod(ex, MethodBase.GetCurrentMethod().Name, _serviceContainerName, request);
+				throw;
+			}
+		}
+
+		public async Task<Database.Response> UpdateRouteAsync(Database.Route request)
+		{
+			if (request is null)
+			{
+				_logHelper.LogNullException(nameof(request));
+				throw new ArgumentNullException(nameof(request));
+			}
+
+			try
+			{
+				_logHelper.LogInvokingGrpcMethod(MethodBase.GetCurrentMethod().Name, _serviceContainerName, request);
+				return await _databaseGrpcServiceClient.UpdateRouteAsync(request);
+			}
+			catch (Exception ex)
+			{
+				_logHelper.LogErrorInvokingGrpcMethod(ex, MethodBase.GetCurrentMethod().Name, _serviceContainerName, request);
+				throw;
+			}
+		}
+
+		public async Task<Database.Response> DeleteRouteAsync(Database.Request request)
+		{
+			if (request is null)
+			{
+				_logHelper.LogNullException(nameof(request));
+				throw new ArgumentNullException(nameof(request));
+			}
+
+			try
+			{
+				_logHelper.LogInvokingGrpcMethod(MethodBase.GetCurrentMethod().Name, _serviceContainerName, request);
+				return await _databaseGrpcServiceClient.DeleteRouteAsync(request);
+			}
+			catch (Exception ex)
+			{
+				_logHelper.LogErrorInvokingGrpcMethod(ex, MethodBase.GetCurrentMethod().Name, _serviceContainerName, request);
+				throw;
+			}
+		}
+
+		public async Task<Database.Response> CreateStopAsync(Database.Stop request)
+		{
+			if (request is null)
+			{
+				_logHelper.LogNullException(nameof(request));
+				throw new ArgumentNullException(nameof(request));
+			}
+
+			try
+			{
+				_logHelper.LogInvokingGrpcMethod(MethodBase.GetCurrentMethod().Name, _serviceContainerName, request);
+				return await _databaseGrpcServiceClient.CreateStopAsync(request);
+			}
+			catch (Exception ex)
+			{
+				_logHelper.LogErrorInvokingGrpcMethod(ex, MethodBase.GetCurrentMethod().Name, _serviceContainerName, request);
+				throw;
+			}
+		}
+
+		public async Task<Database.Stop> GetStopAsync(Database.Request request)
+		{
+			if (request is null)
+			{
+				_logHelper.LogNullException(nameof(request));
+				throw new ArgumentNullException(nameof(request));
+			}
+
+			try
+			{
+				_logHelper.LogInvokingGrpcMethod(MethodBase.GetCurrentMethod().Name, _serviceContainerName, request);
+				return await _databaseGrpcServiceClient.GetStopAsync(request);
+			}
+			catch (Exception ex)
+			{
+				_logHelper.LogErrorInvokingGrpcMethod(ex, MethodBase.GetCurrentMethod().Name, _serviceContainerName, request);
+				throw;
+			}
+		}
+
+		public async Task<Database.StopList> GetAllStopsAsync(Database.Request request)
+		{
+			if (request is null)
+			{
+				_logHelper.LogNullException(nameof(request));
+				throw new ArgumentNullException(nameof(request));
+			}
+
+			try
+			{
+				_logHelper.LogInvokingGrpcMethod(MethodBase.GetCurrentMethod().Name, _serviceContainerName, request);
+				return await _databaseGrpcServiceClient.GetAllStopsAsync(request);
+			}
+			catch (Exception ex)
+			{
+				_logHelper.LogErrorInvokingGrpcMethod(ex, MethodBase.GetCurrentMethod().Name, _serviceContainerName, request);
+				throw;
+			}
+		}
+
+		public async Task<Database.Response> UpdateStopAsync(Database.Stop request)
+		{
+			if (request is null)
+			{
+				_logHelper.LogNullException(nameof(request));
+				throw new ArgumentNullException(nameof(request));
+			}
+
+			try
+			{
+				_logHelper.LogInvokingGrpcMethod(MethodBase.GetCurrentMethod().Name, _serviceContainerName, request);
+				return await _databaseGrpcServiceClient.UpdateStopAsync(request);
+			}
+			catch (Exception ex)
+			{
+				_logHelper.LogErrorInvokingGrpcMethod(ex, MethodBase.GetCurrentMethod().Name, _serviceContainerName, request);
+				throw;
+			}
+		}
+
+		public async Task<Database.Response> DeleteStopAsync(Database.Request request)
+		{
+			if (request is null)
+			{
+				_logHelper.LogNullException(nameof(request));
+				throw new ArgumentNullException(nameof(request));
+			}
+
+			try
+			{
+				_logHelper.LogInvokingGrpcMethod(MethodBase.GetCurrentMethod().Name, _serviceContainerName, request);
+				return await _databaseGrpcServiceClient.DeleteStopAsync(request);
+			}
+			catch (Exception ex)
+			{
+				_logHelper.LogErrorInvokingGrpcMethod(ex, MethodBase.GetCurrentMethod().Name, _serviceContainerName, request);
+				throw;
+			}
+		}
+	}
+}
